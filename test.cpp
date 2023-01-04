79dfcef7e6398af626c543789cffc72254f5c4b6
Justus Dieckmann <justusdieckmann@gmail.com>
2023-01-04 03:32:30 +0100

    Add better cli options

--- test.cpp
+++ test.cpp
@@ -9,42 +9,64 @@ static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
-int main(int argc, const char** argv) {
+void exitWithUsage() {
+    std::cerr << "Usage: ./test [-d <xdim> <ydim> <zdim>] [-g <nGPUs>] [-n <iterations>] [-e <exportFile>]" << std::endl;
+    exit(-1);
+}
+
+int getIntArg(char* s) {
+    int i = std::atoi(s);
+    if (i <= 0) {
+        exitWithUsage();
+    }
+    return i;
+}
+
+int main(int argc, char** argv) {
     vec3<int> size {100, 100, 100};
-
-    size_t gpus = 1;
-
-    bool doExport = true;
-
-    if (argc > 1) {
-        if (argc != 5) {
-            std::cerr << "usage: ./test <xdim> <ydim> <zdim> <nGPUs>" << std::endl;
-            std::exit(-1);
+    int gpus = 1;
+    int iterations = 1;
+    std::string importFile, exportFile;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            exitWithUsage();
         }
-
-        size.x = std::atoi(argv[1]);
-        size.y = std::atoi(argv[2]);
-        size.z = std::atoi(argv[3]);
-        gpus = std::atoi(argv[4]);
+        switch(argv[i++][1]) {
+            case 'd':
+                if (argc < i + 3) {
+                    exitWithUsage();
+                }
+                size.x = getIntArg(argv[i++]);
+                size.y = getIntArg(argv[i++]);
+                size.z = getIntArg(argv[i]);
+                break;
+            case 'g':
+                gpus = getIntArg(argv[i]);
+                break;
+            case 'n':
+                iterations = getIntArg(argv[i]);
+                break;
+            case 'e':
+                exportFile = std::string(argv[i]);
+                break;
+            default:
+                exitWithUsage();
+        }
     }
 
     initSimulation(size.x, size.y, size.z, gpus);
 
     Timer t = Timer();
 
-    if (doExport) {
-        exportFrame("0.bin");
-    }
-
     t.start();
 
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < iterations; i++) {
         simulateStep();
         std::cout << t.round() << std::endl;
     }
 
-    if (doExport) {
-        exportFrame("export.bin");
+    if (!exportFile.empty()) {
+        exportFrame(exportFile);
     }
 
 }
